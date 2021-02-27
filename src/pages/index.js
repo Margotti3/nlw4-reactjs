c047d245cbc64768f2f4b802c03812c30e389f0c
Margotti3 <mmargottia@gmail.com>
2021-02-27 15:58:34 -0300

    changed

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { ExperienceBar } from '../components/ExperienceBar.tsx';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
