83487ff7ed90654d990cd9af8a130d10a4fe0feb
Margotti3 <mmargottia@gmail.com>
2021-02-27 18:39:03 -0300

    third day completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,31 @@
-import { ExperienceBar } from '../components/ExperienceBar.tsx';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Profile } from '../components/Profile';
+import { Countdown } from '../components/Countdown';
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
