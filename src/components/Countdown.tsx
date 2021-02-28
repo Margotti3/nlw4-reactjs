1498657bc0fd26e0b435eb5d09e782938354b118
Margotti3 <mmargottia@gmail.com>
2021-02-28 12:35:04 -0300

    third day completed correction

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Countdown.module.css';
 
+let countDownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
-    const [time, setTime] = useState(25*60);
-    const [active, setActive] = useState(false);
+    const {startNewChallenge} = useContext(ChallengeContext);
+
+    const [time, setTime] = useState(3);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
 
     const minutes = Math.floor(time/60);
     const seconds = time%60;
@@ -12,16 +18,27 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
+    }
+
+    function resetCountdown() {
+        setIsActive(false);
+        clearTimeout(countDownTimeout);
+        setTime(25*60);
     }
 
     useEffect(() => {
-        if(active && time > 0){
-            setTimeout(() => {
+        if(isActive && time > 0){
+            countDownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000);
         }
-    }, [active, time]);
+        else if(isActive && time == 0){
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive, time]);
 
     return(
         <div>
@@ -37,13 +54,36 @@ export function Countdown() {
                 </div>
             </div>
 
-            <button 
-                type="button" 
-                onClick={startCountdown} 
+            {hasFinished ? (
+                <button 
+                disabled
                 className={styles.countdownButton}
-            >
-                Iniciar um ciclo
-            </button>
+                >
+                    Ciclo encerrado
+                </button>
+            ) : (
+                <>
+                {!isActive ? (
+                    <button 
+                        type="button" 
+                        onClick={startCountdown} 
+                        className={styles.countdownButton}
+                    >
+                        Iniciar um ciclo
+                    </button>
+                ) : (
+                    <button 
+                        type="button" 
+                        onClick={resetCountdown} 
+                        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                    >
+                        Abandonar ciclo
+                    </button>
+                )}
+                </>
+            )}
+
+            
         </div>
     );
 }
--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+    const {currentExperience, experienceNextLevel} = useContext(ChallengeContext);
+
+    const percentToNextLevel = Math.round(currentExperience/experienceNextLevel*100);
+
     return (
         <header className={styles.experienceBar}>
             <span>0px</span>
             <div>
-                <div style={{ width: "60%" }} />
-                <span className={styles.currentExperience} style={{ left: "60%"}}>
-                    360px
+                <div style={{ width: `${percentToNextLevel}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%`}} >
+                    {currentExperience} px
                 </span>
             </div>
-            <span>600px</span>
+            <span>{experienceNextLevel} px</span>
         </header>
     );
 }
